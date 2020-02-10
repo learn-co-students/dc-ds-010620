3c5a63ce6c4bdfa366ebbe91d38460f938f0130c
Murat Guner <murat.guner@flatironschool.com>
2020-02-10 14:30:39 -0500

    two sample tests are added

---
+++ module-3/day-6-Two_sample_tests/statistical_inference_about_means_and_proprotions/welch_test_for_two_samples/supplement.py
@@ -0,0 +1,7 @@
+Mathematically we can express this as:
+
+\begin{equation}
+    H_{a}:  \mu_{1} - \mu_{2} > D_{0} = 0\\
+    H_{0}:  \mu_{1} - \mu_{2} \leq D_{0} = 0 \\
+    \text{Significance Level: }\alpha = 0.05
+\end{equation}
